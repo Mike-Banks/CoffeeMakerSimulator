940ad13eb3c312464ba7ebd73cf038c66b9b322e
Brendan V <25186349+vincellb@users.noreply.github.com>
2018-11-25 17:14:54 -0500

    Added comments to mike's shit code

--- app/src/main/java/com/example/mikebanks/coffeemaker/MainActivity.java
+++ app/src/main/java/com/example/mikebanks/coffeemaker/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // field vars for the text views
     private TextView txtBoilerStatus;
     private TextView txtWarmerPlateStatus;
     private TextView txtPressureReliefValveStatus;
@@ -15,14 +17,18 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtBrewingStatus;
     private TextView txtMessage;
 
+
     private Button btnStartBrewing;
     private Button btnPot;
 
+    // listener for the start brewing button
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            // check if the brew btn is pressed
             if (view.getId() == btnStartBrewing.getId()) {
 
+                // check if its not pressed
             } else if (view.getId() == btnPot.getId()) {
 
             }
@@ -34,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // set references to the text boxes
         txtBoilerStatus = findViewById(R.id.txt_boiler_status);
         txtWarmerPlateStatus = findViewById(R.id.txt_plate_status);
         txtPressureReliefValveStatus = findViewById(R.id.txt_valve_status);
@@ -41,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         txtBrewingStatus = findViewById(R.id.txt_brewing_status);
         txtMessage = findViewById(R.id.txt_message);
 
+        // set references to the btns
         btnStartBrewing = findViewById(R.id.btn_start_brewing);
         btnPot = findViewById(R.id.btn_pot);
 
